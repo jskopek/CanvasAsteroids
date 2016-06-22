ebe749cd0fac052db57b0bf0f4e92f4995fb0159
Jean-Marc Skopek <jskopek@gmail.com>
2016-06-22 14:00:37 +0200

    Introduce accelearation and ability to increase/decrease with key presses

--- main.js
+++ main.js
@@ -3,24 +3,42 @@ $(function() {
         x: 200,
         y: 200,
         rotation: 0,
+        acceleration: 1,
+        maxSpeed: 5,
         canvasWidth: 400,
         canvasHeight: 400
     };
 
+    // initialize sprites
     var sprites = {
         ship: new Image()
     }
     sprites.ship.src = 'spaceship.png';
+    // end sprites initialize
 
+    // track input
+    var input = {
+        forward: false
+    };
+    document.onkeydown = function(evt) {
+        evt = evt || window.event;
+        if(evt.keyCode == 38) { input.forward = true; }
+    }
+    document.onkeyup = function(evt) {
+        evt = evt || window.event;
+        if(evt.keyCode == 38) { input.forward = false; }
+    }
+    // end input track
 
     setInterval(function() {
-        updateValues(values);
+        updateValues(values, input);
         drawSpaceship(values, sprites);
     }, 1000 / 60);
 });
 
-function updateValues(values) {
-    values.x += 5;
+function updateValues(values, input) {
+    values.y -= values.acceleration * values.maxSpeed;
+    values.acceleration *= (input.forward && values.acceleration < 1) ? 1.2 : 0.95;
 
     // set constraints
     if (values.x > values.canvasWidth) { values.x = 0; }
